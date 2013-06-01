68254fc746096ab568d81020f87360ca8174518a
Jordan Leigh <AlwaysBCoding@gmail.com>
2013-06-01 03:03:25 -0500

    Final Configuration adding user table

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ActiveRecord::Base
+# ASSOCIATIONS
+
+# VALIDATIONS
+
+# SPECIAL FEATURES
+	has_secure_password
+
+# SCOPES
+
+# DELEGATIONS
+
+# CALLBACKS
+
+# CONFIG METHODS
+	def to_s
+	end
+
+	def to_param
+	end
+
+# CLASS METHODS
+
+# INSTANCE METHODS
+
+# PRIVATE METHODS
+private
+
+end
