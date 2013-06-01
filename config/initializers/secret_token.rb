9c2f6f31bc8b92b85219919e2e7a9c14914895f1
Jordan Leigh <AlwaysBCoding@gmail.com>
2013-06-01 02:21:41 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poolboys::Application.config.secret_key_base = '5f0d6777d64712d1ca612d9c450eb3c8803d321ac9a4169e5fc4b70762baf5e41cea6540f547abc23982ed7af334a129ee06165e5440604b44339751190b842f'
